61cedb9cda920dc6b9eefb772d7156405cab8e12
Yue Yu <yue.yu.yy@outlook.com>
2024-11-09 14:20:46 +0800

    测试git提交

--- gym_pyf16_env/envs/grid_world.py
+++ gym_pyf16_env/envs/grid_world.py
@@ -50,28 +50,9 @@ class GridWorldEnv(gym.Env):
             }
         )
 
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        i.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        # self._action_to_direction = {
-        #     Actions.right.value: np.array([1, 0]),
-        #     Actions.up.value: np.array([0, 1]),
-        #     Actions.left.value: np.array([-1, 0]),
-        #     Actions.down.value: np.array([0, -1]),
-        # }
-
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
-        """
-        If human-rendering is used, `self.window` will be a reference
-        to the window that we draw to. `self.clock` will be a clock that is used
-        to ensure that the environment is rendered at the correct framerate in
-        human-mode. They will remain `None` until human-mode is used for the
-        first time.
-        """
         self.window = None
         self.clock = None
 
